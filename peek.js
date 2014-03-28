db59532e3d145a9a416978c3ae7ff408e41eb14b
Mark Macdonald <mark.t.macdonald@googlemail.com>
2014-03-28 17:02:37 +0100

    added some tutorial links.

--- peek.js
+++ peek.js
@@ -1,4 +1,15 @@
 
+//http://mikemcdearmon.com/portfolio/techposts/charting-libraries-using-d3
+//http://www.recursion.org/d3-for-mere-mortals/
+//http://exposedata.com/tutorial/d3/
+//http://chimera.labs.oreilly.com/books/1230000000345/index.html
+//http://alignedleft.com/tutorials/d3
+//http://techslides.com/over-1000-d3-js-examples-and-demos/
+
+//http://code.shutterstock.com/rickshaw/
+//http://nvd3.org/
+//http://dimplejs.org/
+
 $( document ).ready(function() {
 	d3.select("body").append("span").text("Hello, world!");
 });
