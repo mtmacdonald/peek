2088b9584b715f1b15465968ac17b5896194891c
Mark Macdonald <mark.t.macdonald@googlemail.com>
2014-11-14 15:18:21 +0100

    fixed regression in pie and compare labels in IE.

--- peek.js
+++ peek.js
@@ -60,7 +60,7 @@ function Pie(container) {
                 return "translate(" + self.arc.centroid(d) + ")";
             })
             .attr("text-anchor", "middle")
-            .html(function(d, i) { 
+            .text(function(d, i) { 
                 var value = data[i].value;
                 if (value > 3.0) {
                    return Math.round(data[i].value)+'%';
@@ -96,11 +96,9 @@ function Trend(container, width, height) {
     this.plot;
     this.svg;
 
-    this.interpolate = 'cardinal';
+    this.interpolate = 'basis';
 
-    this.showTooltip = false;
-
-    this.margin = {top: 20, right: 20, bottom: 50, left: 50};
+    this.margin = {top: 0, right: 20, bottom: 50, left: 50};
     this.width = width - this.margin.left - this.margin.right;
     this.height = height - this.margin.top - this.margin.bottom;
 
@@ -144,62 +142,6 @@ function Trend(container, width, height) {
             .style("stroke", metric.colour)
             .attr("d", this.line(metric.values));
     }
-
-    this.render_circles = function(metric, i) {
-        var that = this;
-
-        d3.select(this.container)
-            .append("div")
-            .attr("class", "infobox").html("<p>Tooltip</p>");
-
-        this.svg.selectAll(".plot")
-            .data(metric.values)
-            .enter()
-            .append("circle")
-              .attr("transform", function(d) { 
-                return "translate(" + that.x(d.date) + ", " + that.y(d.value) + ")"; 
-            })
-              .attr("r", function(d){ return 4; }) 
-              .attr("fill", "white")
-              .style("stroke", metric.colour)
-              .on("mouseover", this.mouseover_circle)
-              .on("mouseout", this.mouseout_circle);
-
-        this.plot.on("mousemove", function(){
-            var infobox = d3.select(".infobox");
-            var coord = d3.mouse(this);
-            infobox.style("left", (d3.event.pageX) + 15 + "px" );
-            infobox.style("top", (d3.event.pageY) + "px");
-        });
-    }
-
-    this.mouseover_circle = function(data,i) {     
-        var formatDate = d3.time.format("%A %d. %B %Y");
-        var circle = d3.select(this);
-        //circle.attr("r", function(d){ return 8; });
-        circle.transition().duration(500).attr("r", 16);
-
-        d3.select(".infobox")
-        .style("display", "block")
-        .style('opacity', 0)
-        .transition().delay(200).duration(500).style('opacity', 1);  
-          
-        d3.select(".infobox p")
-            .html("<strong>Date:</strong> " 
-                + formatDate(new Date(data.date)) 
-                + "<br/>" 
-                + "<strong>Value:</strong> " 
-                + data.value
-                );
-    }
-
-    this.mouseout_circle = function() {
-        var circle = d3.select(this);
-        //circle.attr("r", function(d){ return 4; });
-        circle.transition().duration(500).attr("r", 4);
-        d3.select(".infobox").style("display", "none"); 
-    }
-
 
     this.render_x_axis = function() {
         this.svg.append("g")
@@ -278,8 +220,6 @@ function Trend(container, width, height) {
         //plot values
         data.forEach(function(metric, i) {
             this.render_line(metric, i);
-            if(this.showTooltip)
-                this.render_circles(metric, i);
             this.append_to_legend(metric, i);
         }, this);
 
@@ -352,7 +292,7 @@ function Compare(container) {
                 .enter()
                 .append("text")
                     .attr('class', 'compare-chart-values')
-                    .html( function(d) { return d.value.toFixed(2); })
+                    .text( function(d) { return d.value.toFixed(2); })
                     .attr("x", function(d, i) {
                         //position the values just left of the end of the bars
                         var width = this.getComputedTextLength() + 10;
