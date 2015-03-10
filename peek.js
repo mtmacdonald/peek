a9635144c0b887c4c37f7df4a5aa217bfed37533
Mark Macdonald <mark.t.macdonald@googlemail.com>
2015-03-10 17:23:16 +0100

    removed dead code.

--- peek.js
+++ peek.js
@@ -410,13 +410,6 @@ function Cartesian(container, stacked) {
 var groupedData = [
     {
         "State": "AL",
-        "Under 5 Years": "310504",
-        "5 to 13 Years": "552339",
-        "14 to 17 Years": "259034",
-        "18 to 24 Years": "450818",
-        "25 to 44 Years": "1231572",
-        "45 to 64 Years": "1215966",
-        "65 Years and Over": "641667",
         "columnDetails": [
             {
                 "name": "Under 5 Years",
@@ -465,13 +458,6 @@ var groupedData = [
     },
     {
         "State": "AK",
-        "Under 5 Years": "52083",
-        "5 to 13 Years": "85640",
-        "14 to 17 Years": "42153",
-        "18 to 24 Years": "74257",
-        "25 to 44 Years": "198724",
-        "45 to 64 Years": "183159",
-        "65 Years and Over": "50277",
         "columnDetails": [
             {
                 "name": "Under 5 Years",
@@ -520,13 +506,6 @@ var groupedData = [
     },
     {
         "State": "AZ",
-        "Under 5 Years": "515910",
-        "5 to 13 Years": "828669",
-        "14 to 17 Years": "362642",
-        "18 to 24 Years": "601943",
-        "25 to 44 Years": "1804762",
-        "45 to 64 Years": "1523681",
-        "65 Years and Over": "862573",
         "columnDetails": [
             {
                 "name": "Under 5 Years",
@@ -575,13 +554,6 @@ var groupedData = [
     },
     {
         "State": "AR",
-        "Under 5 Years": "202070",
-        "5 to 13 Years": "343207",
-        "14 to 17 Years": "157204",
-        "18 to 24 Years": "264160",
-        "25 to 44 Years": "754420",
-        "45 to 64 Years": "727124",
-        "65 Years and Over": "407205",
         "columnDetails": [
             {
                 "name": "Under 5 Years",
@@ -630,13 +602,6 @@ var groupedData = [
     },
     {
         "State": "CA",
-        "Under 5 Years": "2704659",
-        "5 to 13 Years": "4499890",
-        "14 to 17 Years": "2159981",
-        "18 to 24 Years": "3853788",
-        "25 to 44 Years": "10604510",
-        "45 to 64 Years": "8819342",
-        "65 Years and Over": "4114496",
         "columnDetails": [
             {
                 "name": "Under 5 Years",
@@ -685,13 +650,6 @@ var groupedData = [
     },
     {
         "State": "CO",
-        "Under 5 Years": "358280",
-        "5 to 13 Years": "587154",
-        "14 to 17 Years": "261701",
-        "18 to 24 Years": "466194",
-        "25 to 44 Years": "1464939",
-        "45 to 64 Years": "1290094",
-        "65 Years and Over": "511094",
         "columnDetails": [
             {
                 "name": "Under 5 Years",
@@ -740,13 +698,6 @@ var groupedData = [
     },
     {
         "State": "CT",
-        "Under 5 Years": "211637",
-        "5 to 13 Years": "403658",
-        "14 to 17 Years": "196918",
-        "18 to 24 Years": "325110",
-        "25 to 44 Years": "916955",
-        "45 to 64 Years": "968967",
-        "65 Years and Over": "478007",
         "columnDetails": [
             {
                 "name": "Under 5 Years",
